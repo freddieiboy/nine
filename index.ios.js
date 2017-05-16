ab5bd0e9e42f7755da1874d00b81aa76e019e895
Freddie Iboy <freddieiboy@gmail.com>
2017-05-15 18:02:28 -0700

    9 camera structure

--- index.ios.js
+++ index.ios.js
@@ -8,50 +8,47 @@ import {
   TouchableHighlight,
   View
 } from 'react-native';
-import Camera from 'react-native-camera';
+import CameraContainer from './src/CameraContainer';
 
 class nine extends Component {
+  state = {
+    items: [0,1,2]
+  }
   render() {
+    const createCameraBoxes = this.state.items.map((i, key) => {
+      return (
+        <View style={styles.mediaBox} key={key}>
+          <CameraContainer/>
+        </View>
+      )
+    });
+    const createRow = this.state.items.map((i, key) => {
+      return (
+        <View style={styles.row} key={key}>
+          {createCameraBoxes}
+        </View>
+      )
+    });
     return (
       <View style={styles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-        </Camera>
+        {createRow}
       </View>
     );
-  }
-
-  takePicture() {
-    const options = {};
-    //options.location = ...
-    this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
-  preview: {
+  row: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    flexDirection: 'row'
   },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
+  mediaBox: {
+    flex: 1,
+    borderWidth: 1
   }
 });
 
